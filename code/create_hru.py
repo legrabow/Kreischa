0ba2577416573c96a3255dc5ebbeadff131274bc
legrabow <leonard.grabow@mailbox.tu-dresden.de>
2021-11-18 14:36:48 +0100

    finalise create_hru.py

--- code/create_hru.py
+++ code/create_hru.py
@@ -1,28 +1,36 @@
-# Builds spatial subsets based on the land use and soil maps
 import geopandas as gpd
-from geopandas.tools import overlay
-pathToLanduse = "/home/grabow/Dropbox/Daten_Kreischa/Landnutzung/Lfulg/Daten/btlnk_flaechen.shp"
-pathToSoil = "/home/grabow/Dropbox/Uni/Einzugsgebietsmodellierung/Boden/DATEN/BK50.shp"
 
-
-## prepare land use layer
-landuse = gpd.read_file(pathToLanduse)
-levelOfDetail = "HG" # HG, UG or Bestand
-
-if levelOfDetail == "HG":
-    uniqueColumn = landuse.HG.astype(str)
-if levelOfDetail == "UG":
-    uniqueColumn = landuse.HG.astype(str) + landuse.UG.astype(str)
-if levelOfDetail == "Bestand":
-    uniqueColumn = landuse.HG.astype(str) + landuse.UG.astype(str) + landuse.BESTAND.astype(str)
+def main(pathToLanduse, pathToSoil, pathToHru, levelOfDetail):
+    """
+    Builds spatial subsets based on the land use and soil shape features
     
-landuse["uniqueColumn"] = uniqueColumn
-landuseReduced = landuse.dissolve(by = "uniqueColumn")
+    Keyword-arguments
+    pathToLanduse -- total path to the land use layer
+    pathToSoil -- total path to soil layer
+    pathToHru -- total path where the produced hru layer will be written
+    levelOfDetail -- level of detail for the land use layer. Either use "HG", "UG", "Bestand"
+    """
+   
+    ## prepare land use layer
+    landuse = gpd.read_file(pathToLanduse)
+    levelOfDetail = "HG" # HG, UG or Bestand
 
-## prepare soil layer
-soil = gpd.read_file(pathToSoil)
-soilReduced = soil.dissolve(by = "BOTYP")
+    if levelOfDetail == "HG":
+        uniqueColumn = landuse.HG.astype(str)
+    if levelOfDetail == "UG":
+        uniqueColumn = landuse.HG.astype(str) + landuse.UG.astype(str)
+    if levelOfDetail == "Bestand":
+        uniqueColumn = landuse.HG.astype(str) + landuse.UG.astype(str) + landuse.BESTAND.astype(str)
 
-## create HRUs
-hru = soilReduced.overlay(landuseReduced, how='union')
-hru.to_file("/home/grabow/Dropbox/Daten_Kreischa/Hydrotope/hru.shp")
+    landuse["uniqueColumn"] = uniqueColumn
+    landuseReduced = landuse.dissolve(by = "uniqueColumn")
+    landuseReduced.drop(landuseReduced.columns.difference(['uniqueColumn','geometry']), 1, inplace=True)
+
+    ## prepare soil layer
+    soil = gpd.read_file(pathToSoil)
+    soilReduced = soil.dissolve(by = "BOTYP")
+    soilReduced.drop(soilReduced.columns.difference(['BOTYP','geometry']), 1, inplace=True)
+
+    ## create HRUs
+    hru = soilReduced.overlay(landuseReduced, how='union')
+    hru.to_file(pathToHru)
